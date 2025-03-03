d2dc6aadb9b08ae34e7c7ca55ef026e20318296a
Filipe Esch <filipeesch@hotmail.com>
2025-03-03 20:53:18 +0000

    teste

--- components/aircon_climate/__init__.py
+++ components/aircon_climate/__init__.py
@@ -21,5 +21,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], uart_var)
 
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    # await uart.register_uart_device(var, config)
     await climate.register_climate(var, config)
